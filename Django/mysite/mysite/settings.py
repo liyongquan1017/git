d3aac66ca8b547550ea2e7366c3a0bec076c8b20
李永泉 <liyongquan@liyongquandeAir.lan>
2018-08-03 23:53:37 +0800

    newDjango

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y^&n)gkovyzjj2rvg(pyh1=cxk_mx6%xz=2m^9bey0+z^+=^r!'
+SECRET_KEY = '0kdsjk2mfb%znxn10&237(e@#6q(%sk4yrw8a@e#2q0h01(9!z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
